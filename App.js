3e4092e6f6e8cb626d6e01f5b4d1dcc24b12384c
Ahmad Rasool Malik <ahmadmlk16@vt.edu>
2020-10-25 00:19:22 -0400

    Started implementing the login/register landing page. Used base code obtained from open source github repo for base functionality. Styling still not working in this push login info not linked with data base as of yet

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import "./App.css";
+import { Login, Register } from "./login/";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLogginActive: true
+        };
+    }
+
+    componentDidMount() {
+        //Add .right by default
+        this.rightSide.classList.add("right");
+    }
+
+    changeState() {
+        const { isLogginActive } = this.state;
+
+        if (isLogginActive) {
+            this.rightSide.classList.remove("right");
+            this.rightSide.classList.add("left");
+        } else {
+            this.rightSide.classList.remove("left");
+            this.rightSide.classList.add("right");
+        }
+        this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
+    }
+
+    render() {
+        const { isLogginActive } = this.state;
+        const current = isLogginActive ? "Register" : "Login";
+        const currentActive = isLogginActive ? "login" : "register";
+        return (
+            <div className="App">
+                <div className="login">
+                    <div className="container" ref={ref => (this.container = ref)}>
+                        {isLogginActive && (
+                            <Login containerRef={ref => (this.current = ref)} />
+                        )}
+                        {!isLogginActive && (
+                            <Register containerRef={ref => (this.current = ref)} />
+                        )}
+                    </div>
+                    <RightSide
+                        current={current}
+                        currentActive={currentActive}
+                        containerRef={ref => (this.rightSide = ref)}
+                        onClick={this.changeState.bind(this)}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const RightSide = props => {
+    return (
+        <div
+            className="right-side"
+            ref={props.containerRef}
+            onClick={props.onClick}
+        >
+            <div className="inner-container">
+                <div className="text">{props.current}</div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
